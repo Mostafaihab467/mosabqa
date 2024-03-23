e4ca7370b2f450d4e9257dcd64d6e8950f3ea0c3
Mgahed <abdelrhmanmgahed131@gmail.com>
2024-03-24 01:31:54 +0200

    logger

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
 
         /*
          * Application Service Providers...
