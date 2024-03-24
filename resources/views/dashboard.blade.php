8ff0f995e5cc357ce997d29a4abbd86677e669be
Mgahed <abdelrhmanmgahed131@gmail.com>
2024-03-24 19:08:57 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,7 @@
             @php
                 $userQuestionsCount = \DB::table('user_question_answers')->where('user_id', Auth::id())->where('category_id', Auth::user()->category_id)->count();
                 $answeredQuestionsCount = \DB::table('user_question_answers')->where('user_id', Auth::id())->where('category_id', Auth::user()->category_id)->whereNotNull('is_correct')->count();
+                $categories = \DB::table('user_question_answers')->where('user_id', Auth::id())->pluck('category_id')->unique();
             @endphp
             <div class="container-fluid h-100">
                 @role('student')
@@ -19,36 +20,41 @@
                             </span>
                         </h1>
                     @else
-                        @if(Auth::user()->grade && Auth::user()->grade >= \App\Models\Lookup::where('name', 'success_percentage')->first()->value)
-                            <h1>
-                                <span class='text-success'>
-                                    {{__('admin.Congratulations, you have passed the exam with grade') . " " . getDgree(auth()->id())}}%
-                                </span>
-                            </h1>
-                            <h1>
-                                {{__('admin.Your serial number is')}} {{Auth::user()->serial}}
-                            </h1>
-                        @elseif(is_null(Auth::user()->grade))
-                            <h1>
-                                {{__('admin.You have not taken the test yet')}}
-                                <a href="{{route('student.questions')}}" class="btn btn-primary">
-                                    {{__('admin.Start test')}}
-                                </a>
-                            </h1>
-                        @else
-                            @if($userQuestionsCount == $answeredQuestionsCount)
-                                <h1 class="text-danger">
-                                    {{__('admin.You have failed the test')}}
-                                </h1>
-                                <h1 class="text-danger">
-                                    {{__('admin.Your grade is')}} {{Auth::user()->grade}}
-                                </h1>
-                            @else
+                        @if($categories->count() <= 1)
                             <a href="{{route('student.questions')}}" class="btn btn-primary">
                                 {{__('admin.Start test')}}
                             </a>
+                        @else
+                            @if(Auth::user()->grade && Auth::user()->grade >= \App\Models\Lookup::where('name', 'success_percentage')->first()->value)
+                                <h1>
+                                    <span class='text-success'>
+                                        {{__('admin.Congratulations, you have passed the exam with grade') . " " . getDgree(auth()->id())}}%
+                                    </span>
+                                </h1>
+                                <h1>
+                                    {{__('admin.Your serial number is')}} {{Auth::user()->serial}}
+                                </h1>
+                            @elseif(is_null(Auth::user()->grade))
+                                <h1>
+                                    {{__('admin.You have not taken the test yet')}}
+                                    <a href="{{route('student.questions')}}" class="btn btn-primary">
+                                        {{__('admin.Start test')}}
+                                    </a>
+                                </h1>
+                            @else
+                                @if($userQuestionsCount == $answeredQuestionsCount)
+                                    <h1 class="text-danger">
+                                        {{__('admin.You have failed the test')}}
+                                    </h1>
+                                    <h1 class="text-danger">
+                                        {{__('admin.Your grade is')}} {{Auth::user()->grade}}
+                                    </h1>
+                                @else
+                                <a href="{{route('student.questions')}}" class="btn btn-primary">
+                                    {{__('admin.Start test')}}
+                                </a>
+                                @endif
                             @endif
-                        @endif
                     @endif
                 @else
                     <h1>
